818384e85f19045c836e5b44502d7694e0d51dc4
Student <Student@PC83>
2017-10-23 11:32:04 +0800

    this is a testing use  class....

---
+++ src/Lav.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class Lav {
+    private String Jc15;
+    private int jj;
+}
